38185c975265e79007328d15fdf64e0a5aab751e
decobot <capy@deco.cx>
2025-06-04 12:53:41 +0000

    ajustes nos componentes de imagem da `home`

--- sections/Category/CategoryGrid.tsx
+++ sections/Category/CategoryGrid.tsx
@@ -5,8 +5,9 @@ import Section, {
 } from "../../components/ui/Section.tsx";
 import Slider from "../../components/ui/Slider.tsx";
 import { clx } from "../../sdk/clx.ts";
-import { useDevice } from "@deco/deco/hooks";
 import { type LoadingFallbackProps } from "@deco/deco";
+import Icon from "../../components/ui/Icon.tsx";
+import { useId } from "../../sdk/useId.ts";
 /** @titleBy label */
 export interface Item {
   image: ImageWidget;
@@ -19,7 +20,7 @@ export interface Props extends SectionHeaderProps {
 function Card({ image, href, label }: Item) {
   return (
     <a href={href} class="flex flex-col items-center justify-center gap-4">
-      <div class="w-44 h-44 rounded-full bg-base-200 flex justify-center items-center border border-transparent hover:border-primary">
+      <div class="w-44 h-44 rounded-full bg-white flex justify-center items-center border border-base-200 hover:border-primary">
         <Image
           src={image}
           alt={label}
@@ -32,42 +33,60 @@ function Card({ image, href, label }: Item) {
     </a>
   );
 }
-function CategoryGrid({ title, cta, items }: Props) {
-  const device = useDevice();
-  return (
-    <Section.Container>
-      <Section.Header title={title} cta={cta} />
+function CategoryGrid({ title, items }: Props) {
+  const id = useId();
 
-      {device === "desktop"
-        ? (
-          <div class="grid grid-cols-6 gap-10">
-            {items.map((i) => <Card {...i} />)}
-          </div>
-        )
-        : (
-          <Slider class="carousel carousel-center sm:carousel-end gap-5 w-full">
-            {items.map((i, index) => (
-              <Slider.Item
-                index={index}
-                class={clx(
-                  "carousel-item",
-                  "first:pl-5 first:sm:pl-0",
-                  "last:pr-5 last:sm:pr-0",
-                )}
-              >
-                <Card {...i} />
-              </Slider.Item>
-            ))}
-          </Slider>
-        )}
-    </Section.Container>
+  return (
+    <div
+      id={id}
+      class="w-full container px-0 py-4 lg:p-4 mx-auto"
+    >
+      {title && <Section.Header title={title} />}
+
+      <div className="relative px-0 lg:px-16">
+        <Slider class="carousel carousel-center gap-4 sm:gap-6 w-full">
+          {items.map((i, index) => (
+            <Slider.Item
+              index={index}
+              class={clx(
+                "carousel-item",
+                "first:pl-4 first:sm:pl-0",
+                "last:pr-4 last:sm:pr-0",
+              )}
+            >
+              <Card {...i} />
+            </Slider.Item>
+          ))}
+        </Slider>
+
+        <div class="hidden sm:flex items-center justify-center z-10 absolute top-1/2 -translate-y-1/2 left-0">
+          <Slider.PrevButton
+            class="hidden sm:flex disabled:invisible btn btn-outline btn-circle text-black hover:text-black no-animation border-0 bg-white hover:bg-white shadow-lg"
+            disabled={false}
+          >
+            <Icon id="chevron-right" size={24} class="rotate-180" />
+          </Slider.PrevButton>
+        </div>
+
+        <div class="hidden sm:flex items-center justify-center z-10 absolute top-1/2 -translate-y-1/2 right-0">
+          <Slider.NextButton
+            class="hidden sm:flex disabled:invisible btn btn-outline btn-circle text-black hover:text-black no-animation border-0 bg-white hover:bg-white shadow-lg"
+            disabled={false}
+          >
+            <Icon id="chevron-right" size={24} />
+          </Slider.NextButton>
+        </div>
+
+        <Slider.JS rootId={id} />
+      </div>
+    </div>
   );
 }
 export const LoadingFallback = (
-  { title, cta }: LoadingFallbackProps<Props>,
+  { title }: LoadingFallbackProps<Props>,
 ) => (
   <Section.Container>
-    <Section.Header title={title} cta={cta} />
+    <Section.Header title={title} />
     <Section.Placeholder height="212px" />;
   </Section.Container>
 );
--- sections/Images/ImageGallery.tsx
+++ sections/Images/ImageGallery.tsx
@@ -1,9 +1,5 @@
 import { type ImageWidget } from "apps/admin/widgets.ts";
 import { Picture, Source } from "apps/website/components/Picture.tsx";
-import Section, {
-  type Props as SectionHeaderProps,
-} from "../../components/ui/Section.tsx";
-import { type LoadingFallbackProps } from "@deco/deco";
 /**
  * @titleBy alt
  */
@@ -15,10 +11,10 @@ interface Banner {
   /** @description Adicione um link */
   href: string;
 }
-interface Props extends SectionHeaderProps {
+interface Props {
   /**
-   * @maxItems 4
-   * @minItems 4
+   * @maxItems 2
+   * @minItems 2
    */
   banners?: Banner[];
 }
@@ -40,7 +36,7 @@ function Banner({ mobile, desktop, alt, href }: Banner) {
         />
         <img
           width={640}
-          class="w-full h-full object-cover"
+          class="w-full h-full object-cover rounded-2xl"
           src={mobile}
           alt={alt}
           decoding="async"
@@ -51,8 +47,6 @@ function Banner({ mobile, desktop, alt, href }: Banner) {
   );
 }
 function Gallery({
-  title,
-  cta,
   banners = [
     {
       mobile:
@@ -69,46 +63,19 @@ function Gallery({
         "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/1125d938-89ff-4aae-a354-63d4241394a6",
       desktop:
         "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/1125d938-89ff-4aae-a354-63d4241394a6",
-    },
-    {
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/dd1e2acb-ff80-49f9-8f56-1deac3b7a42d",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/dd1e2acb-ff80-49f9-8f56-1deac3b7a42d",
-      href: "/",
-      alt: "Fashion",
-    },
-    {
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/0b85ba2d-48b1-4f5b-b619-7f4a7f50b455",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/239/0b85ba2d-48b1-4f5b-b619-7f4a7f50b455",
-      alt: "Fashion",
-      href: "/",
-    },
+    }
   ],
 }: Props) {
   return (
-    <Section.Container>
-      <Section.Header title={title} cta={cta} />
-
-      <ul class="grid gap-2 sm:gap-4 grid-cols-1 sm:grid-cols-2 px-5 sm:px-0">
+    <div class="w-full container p-4 mx-auto">
+      <ul class="grid gap-8 lg:gap-4 sm:gap-8 grid-cols-1 sm:grid-cols-2">
         {banners.map((item) => (
           <li>
             <Banner {...item} />
           </li>
         ))}
       </ul>
-    </Section.Container>
+    </div>
   );
 }
-export const LoadingFallback = (
-  { title, cta }: LoadingFallbackProps<Props>,
-) => (
-  <Section.Container>
-    <Section.Header title={title} cta={cta} />
-
-    <Section.Placeholder height="635px" />;
-  </Section.Container>
-);
 export default Gallery;
