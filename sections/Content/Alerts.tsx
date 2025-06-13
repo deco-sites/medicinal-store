6cfce6a1ddb429274816d6bca73e1bf315405539
decobot <capy@deco.cx>
2025-06-13 16:47:40 +0000

    adiciona novo componente `Alerts` para páginas institucionais

---
+++ sections/Content/Alerts.tsx
@@ -0,0 +1,62 @@
+import Image from "apps/website/components/Image.tsx";
+import { ImageWidget, RichText } from "apps/admin/widgets.ts";
+
+/** @titleBy text */
+export interface Alert {
+  /**
+   * @description 16x16
+   */
+  icon?: ImageWidget;
+  text: RichText;
+}
+
+interface Props {
+  title?: RichText;
+  text?: RichText;
+  alerts: Alert[];
+}
+
+const Card = ({
+  icon,
+  text,
+}: Alert) => (
+  <div class="flex items-center gap-2">
+    {icon && (
+      <Image
+        src={icon}
+        width={16}
+        height={16}
+        loading="lazy"
+      />
+    )}
+    <div
+      class="fluid-text text-sm"
+      dangerouslySetInnerHTML={{ __html: text }}
+    />
+  </div>
+);
+
+export default function Alerts({ title, text, alerts = [] }: Props) {
+  return (
+    <div class="w-full container p-4 mx-auto">
+      {title && title !== "" && (
+        <div class="flex flex-col items-center w-full gap-4">
+          <div class="w-20 h-[3px] bg-primary" />
+          <div
+            class="font-bold text-lg md:text-2xl mb-4 text-center uppercase max-w-72 mx-auto"
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+        </div>
+      )}
+      {text && text !== "" && (
+        <div
+          class="fluid-text max-w-96 mx-auto mb-4 text-center"
+          dangerouslySetInnerHTML={{ __html: text }}
+        />
+      )}
+      <div class="grid md:flex items-center grid-cols-2 md:justify-between">
+        {alerts.map((alert, index) => <Card key={index} {...alert} />)}
+      </div>
+    </div>
+  );
+}
