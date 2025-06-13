b2476b2b999dd6bd69eec60c18fe842ddae263df
decobot <capy@deco.cx>
2025-06-13 17:54:13 +0000

    unifica as respostas dos formulários

--- sections/Form/ContactUs.tsx
+++ sections/Form/ContactUs.tsx
@@ -1,7 +1,11 @@
+import { useId } from "../../sdk/useId.ts";
 import { useScript } from "@deco/deco/hooks";
-import { useComponent } from "../Component.tsx";
-import type { AppContext } from "../../apps/site.ts";
 import { usePlatform } from "../../sdk/usePlatform.tsx";
+import { useComponent } from "../Component.tsx";
+
+import Icon from "../../components/ui/Icon.tsx";
+
+import type { AppContext } from "../../apps/site.ts";
 
 const onLoad = () => {
   (document.querySelector("input[name='phone']") as HTMLInputElement).oninput =
@@ -16,134 +20,185 @@ const onLoad = () => {
     };
 };
 
+// @ts-ignore .
+const loadModal = (id) => {
+  const modal = document.getElementById(id);
+  // @ts-ignore .
+  modal?.showModal();
+};
+
 export default function Form({
-  toast = null,
-  message = "Formulario enviado com sucesso!",
+  status = null,
 }) {
+  const id = useId();
+
   return (
-    <div>
-      <div class="container p-4 mx-auto w-full relative">
-        {toast === "success" && (
-          <div class="toast toast-end">
-            <div class="alert alert-success text-white">
-              <span>{message}</span>
+    <div class="container p-4 mx-auto w-full relative">
+      {status === "success" && (
+        <>
+          <dialog id={id} class="modal">
+            <div class="modal-box">
+              <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 class="flex items-center gap-1 text-lg font-bold">
+                <Icon id="check-success" class="text-green-600" />
+                Inscrição realizada com sucesso!
+              </h3>
+              <p class="text-start">
+                Em breve enviaremos novidades na sua caixa de entrada!
+              </p>
+              <div class="modal-action">
+                <form method="dialog">
+                  <button class="btn btn-primary">
+                    Ok
+                  </button>
+                </form>
+              </div>
             </div>
-          </div>
-        )}
+          </dialog>
+          <script
+            type="text/javascript"
+            defer
+            dangerouslySetInnerHTML={{
+              __html: useScript(loadModal, id),
+            }}
+          />
+        </>
+      )}
 
-        {toast === "error" && (
-          <div class="toast toast-end">
-            <div class="alert alert-error text-white">
-              <span>{message}</span>
+      {status === "failed" && (
+        <>
+          <dialog id={id} class="modal">
+            <div class="modal-box">
+              <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 class="flex items-center gap-1 text-lg font-bold">
+                <Icon id="check-error" class="text-red-700" />
+                Houve um problema.
+              </h3>
+              <p class="text-start">Por favor, tente mais tarde.</p>
             </div>
-          </div>
-        )}
+          </dialog>
+          <script
+            type="text/javascript"
+            defer
+            dangerouslySetInnerHTML={{
+              __html: useScript(loadModal, id),
+            }}
+          />
+        </>
+      )}
 
-        <form
-          class="flex flex-col gap-4 border border-base-200 p-5 rounded-3xl"
-          hx-swap="outerHTML"
-          hx-post={useComponent(import.meta.url)}
-          hx-target="closest section"
-        >
-          <div class="uppercase font-bold text-sm md:text-base">
-            Envie sua mensagem
-          </div>
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="text"
-            name="name"
-            placeholder="Nome*"
-            required
-          />
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="text"
-            name="lastName"
-            placeholder="Sobrenome*"
-            required
-          />
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="email"
-            name="email"
-            placeholder="Email*"
-            required
-          />
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="text"
-            name="phone"
-            placeholder="Celular*"
-            required
-          />
-          <select
-            name="type"
-            class="select select-bordered flex-grow text-sm w-full"
-            required
-          >
-            <option class="text-gray-400" value="">Tipo*</option>
-            <option value="Elogio">Elogio</option>
-            <option value="Sugestão">Sugestão</option>
-            <option value="Dúvida">Dúvida</option>
-            <option value="Reclamação">Reclamação</option>
-          </select>
-          <textarea
-            class="textarea textarea-bordered rounded-2xl min-h-80 w-full placeholder:text-accent"
-            name="description"
-            placeholder="Qual sua dúvida?*"
-            required
-          />
-          <label class="label gap-2 text-sm justify-start p-0">
-            <input
-              required
-              name=""
-              type="checkbox"
-              class="checkbox checkbox-primary"
-            />
-            <span>
-              Estou de acordo com a{" "}
-              <a
-                class="inline font-bold underline"
-                target="_blank"
-                href="/politicas-de-privacidade"
-              >
-                política de privacidade
-              </a>{" "}
-              da Medicinal.
-            </span>
-          </label>
-          <label class="label gap-2 text-sm justify-start p-0">
-            <input
-              name="whatsAppNews"
-              type="checkbox"
-              class="checkbox checkbox-primary"
-            />
-            Quero receber novidades e ofertas por WhatsApp.
-          </label>
-          <label class="label gap-2 text-sm justify-start p-0">
-            <input
-              name="emailNews"
-              type="checkbox"
-              class="checkbox checkbox-primary"
-            />
-            Quero receber novidades e ofertas por E-mail.
-          </label>
-          <button
-            class="btn btn-primary w-full"
-            type="submit"
-          >
-            <span class="[.htmx-request_&]:hidden inline">Enviar</span>
-            <span class="[.htmx-request_&]:inline-block hidden loading loading-spinner" />
-          </button>
-        </form>
-        <script
-          type="text/javascript"
-          defer
-          dangerouslySetInnerHTML={{
-            __html: useScript(onLoad),
-          }}
+      <form
+        class="flex flex-col gap-4 border border-base-200 p-5 rounded-3xl"
+        hx-swap="outerHTML"
+        hx-post={useComponent(import.meta.url)}
+        hx-target="closest section"
+      >
+        <div class="uppercase font-bold text-sm md:text-base">
+          Envie sua mensagem
+        </div>
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="text"
+          name="name"
+          placeholder="Nome*"
+          required
         />
-      </div>
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="text"
+          name="lastName"
+          placeholder="Sobrenome*"
+          required
+        />
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="email"
+          name="email"
+          placeholder="Email*"
+          required
+        />
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="text"
+          name="phone"
+          placeholder="Celular*"
+          required
+        />
+        <select
+          name="type"
+          class="select select-bordered flex-grow text-sm w-full"
+          required
+        >
+          <option class="text-gray-400" value="">Tipo*</option>
+          <option value="Elogio">Elogio</option>
+          <option value="Sugestão">Sugestão</option>
+          <option value="Dúvida">Dúvida</option>
+          <option value="Reclamação">Reclamação</option>
+        </select>
+        <textarea
+          class="textarea textarea-bordered rounded-2xl min-h-80 w-full placeholder:text-accent"
+          name="description"
+          placeholder="Qual sua dúvida?*"
+          required
+        />
+        <label class="label gap-2 text-sm justify-start p-0">
+          <input
+            required
+            name=""
+            type="checkbox"
+            class="checkbox checkbox-primary"
+          />
+          <span>
+            Estou de acordo com a{" "}
+            <a
+              class="inline font-bold underline"
+              target="_blank"
+              href="/politicas-de-privacidade"
+            >
+              política de privacidade
+            </a>{" "}
+            da Medicinal.
+          </span>
+        </label>
+        <label class="label gap-2 text-sm justify-start p-0">
+          <input
+            name="whatsAppNews"
+            type="checkbox"
+            class="checkbox checkbox-primary"
+          />
+          Quero receber novidades e ofertas por WhatsApp.
+        </label>
+        <label class="label gap-2 text-sm justify-start p-0">
+          <input
+            name="emailNews"
+            type="checkbox"
+            class="checkbox checkbox-primary"
+          />
+          Quero receber novidades e ofertas por E-mail.
+        </label>
+        <button
+          class="btn btn-primary w-full"
+          type="submit"
+        >
+          <span class="[.htmx-request_&]:hidden inline">Enviar</span>
+          <span class="[.htmx-request_&]:inline-block hidden loading loading-spinner" />
+        </button>
+      </form>
+      <script
+        type="text/javascript"
+        defer
+        dangerouslySetInnerHTML={{
+          __html: useScript(onLoad),
+        }}
+      />
     </div>
   );
 }
@@ -167,14 +222,9 @@ export const action = async (
         data: JSON.stringify(data),
       });
     }
-    return {
-      toast: "success",
-      message: "Menssagem enviada com sucesso!",
-    };
+
+    return { status: "success" };
   } catch {
-    return {
-      toast: "error",
-      message: "Erro ao enviar a mensagem. Tente novamente mais tarde.",
-    };
+    return { status: "failed" };
   }
 };
--- sections/Form/SendYourRecipe.tsx
+++ sections/Form/SendYourRecipe.tsx
@@ -3,6 +3,7 @@ import { useComponent } from "../Component.tsx";
 import type { AppContext } from "../../apps/site.ts";
 import Icon from "../../components/ui/Icon.tsx";
 import { usePlatform } from "../../sdk/usePlatform.tsx";
+import { useId } from "../../sdk/useId.ts";
 
 const onLoad = () => {
   (document.querySelector("input[name='whatsapp']") as HTMLInputElement)
@@ -27,9 +28,8 @@ const onLoad = () => {
     const fileNameDisplay = document.getElementById("selected-file-name");
     if (fileNameDisplay) {
       if (e.target.files.length > 0) {
-        fileNameDisplay.textContent = `Arquivo selecionado: ${
-          e.target.files[0].name
-        }`;
+        fileNameDisplay.textContent = `Arquivo selecionado: ${e.target.files[0].name
+          }`;
         fileNameDisplay.classList.remove("hidden");
       } else {
         fileNameDisplay.textContent = "";
@@ -39,122 +39,173 @@ const onLoad = () => {
   };
 };
 
+// @ts-ignore .
+const loadModal = (id) => {
+  const modal = document.getElementById(id);
+  // @ts-ignore .
+  modal?.showModal();
+};
+
 export default function Form({
-  toast = null,
-  message = "Formulario enviado com sucesso!",
+  status = null,
 }) {
+  const id = useId();
+
   return (
-    <div>
-      <div class="container p-4 mx-auto w-full relative">
-        {toast === "success" && (
-          <div class="toast toast-end">
-            <div class="alert alert-success text-white">
-              <span>{message}</span>
+    <div class="container p-4 mx-auto w-full relative">
+      {status === "success" && (
+        <>
+          <dialog id={id} class="modal">
+            <div class="modal-box">
+              <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 class="flex items-center gap-1 text-lg font-bold">
+                <Icon id="check-success" class="text-green-600" />
+                Inscrição realizada com sucesso!
+              </h3>
+              <p class="text-start">
+                Em breve enviaremos novidades na sua caixa de entrada!
+              </p>
+              <div class="modal-action">
+                <form method="dialog">
+                  <button class="btn btn-primary">
+                    Ok
+                  </button>
+                </form>
+              </div>
             </div>
-          </div>
-        )}
+          </dialog>
+          <script
+            type="text/javascript"
+            defer
+            dangerouslySetInnerHTML={{
+              __html: useScript(loadModal, id),
+            }}
+          />
+        </>
+      )}
 
-        {toast === "error" && (
-          <div class="toast toast-end">
-            <div class="alert alert-error text-white">
-              <span>{message}</span>
+      {status === "failed" && (
+        <>
+          <dialog id={id} class="modal">
+            <div class="modal-box">
+              <form method="dialog">
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                  ✕
+                </button>
+              </form>
+              <h3 class="flex items-center gap-1 text-lg font-bold">
+                <Icon id="check-error" class="text-red-700" />
+                Houve um problema.
+              </h3>
+              <p class="text-start">Por favor, tente mais tarde.</p>
             </div>
-          </div>
-        )}
+          </dialog>
+          <script
+            type="text/javascript"
+            defer
+            dangerouslySetInnerHTML={{
+              __html: useScript(loadModal, id),
+            }}
+          />
+        </>
+      )}
 
-        <form
-          class="flex flex-col gap-4 border border-base-200 p-5 rounded-3xl"
-          hx-swap="outerHTML"
-          hx-post={useComponent(import.meta.url)}
-          hx-target="closest section"
-        >
-          <div class="uppercase font-bold text-sm md:text-base">
-            Envie sua receita
-          </div>
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="text"
-            name="name"
-            placeholder="Nome*"
-            required
-          />
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="text"
-            name="lastName"
-            placeholder="Sobrenome*"
-            required
-          />
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="email"
-            name="email"
-            placeholder="Email*"
-            required
-          />
-          <input
-            class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
-            type="text"
-            name="whatsapp"
-            placeholder="WhatsApp*"
-            required
-          />
-          <div
-            class="flex flex-col gap-2 items-center border border-base-200 rounded-2xl p-4 text-center cursor-pointer"
-            id="file-selector-area"
-          >
-            <Icon id="file" size={54} class="text-base-200" />
-            <p class="text-black font-bold underline">
-              Tire uma foto ou anexe sua receita
-            </p>
-            <p class="text-xs text-gray-500">(Formatos: jpeg, png e pdf)</p>
-            <input
-              id="file-input"
-              name="prescription"
-              type="file"
-              class="hidden"
-              accept=".jpeg,.jpg,.png,.pdf"
-              required
-            />
-            <p
-              id="selected-file-name"
-              class="mt-2 text-sm text-gray-700 hidden"
-            >
-            </p>
-          </div>
-          <select
-            name="type"
-            class="select select-bordered flex-grow text-sm w-full"
-            required
-          >
-            <option class="text-gray-400" value="">
-              Prefere qual canal de resposta para seu orçamento?*
-            </option>
-            <option value="Elogio">E-mail</option>
-            <option value="Sugestão">WhatsApp</option>
-          </select>
-          <textarea
-            class="textarea textarea-bordered rounded-2xl min-h-80 w-full placeholder:text-accent"
-            name="notes"
-            placeholder="Observações*"
-            required
-          />
-          <button
-            class="btn btn-primary w-full"
-            type="submit"
-          >
-            <span class="[.htmx-request_&]:hidden inline">Enviar</span>
-            <span class="[.htmx-request_&]:inline-block hidden loading loading-spinner" />
-          </button>
-        </form>
-        <script
-          type="text/javascript"
-          defer
-          dangerouslySetInnerHTML={{
-            __html: useScript(onLoad),
-          }}
+      <form
+        class="flex flex-col gap-4 border border-base-200 p-5 rounded-3xl"
+        hx-swap="outerHTML"
+        hx-post={useComponent(import.meta.url)}
+        hx-target="closest section"
+      >
+        <div class="uppercase font-bold text-sm md:text-base">
+          Envie sua receita
+        </div>
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="text"
+          name="name"
+          placeholder="Nome*"
+          required
         />
-      </div>
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="text"
+          name="lastName"
+          placeholder="Sobrenome*"
+          required
+        />
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="email"
+          name="email"
+          placeholder="Email*"
+          required
+        />
+        <input
+          class="input input-bordered flex-grow text-sm w-full placeholder:text-accent"
+          type="text"
+          name="whatsapp"
+          placeholder="WhatsApp*"
+          required
+        />
+        <div
+          class="flex flex-col gap-2 items-center border border-base-200 rounded-2xl p-4 text-center cursor-pointer"
+          id="file-selector-area"
+        >
+          <Icon id="file" size={54} class="text-base-200" />
+          <p class="text-black font-bold underline">
+            Tire uma foto ou anexe sua receita
+          </p>
+          <p class="text-xs text-gray-500">(Formatos: jpeg, png e pdf)</p>
+          <input
+            id="file-input"
+            name="prescription"
+            type="file"
+            class="hidden"
+            accept=".jpeg,.jpg,.png,.pdf"
+            required
+          />
+          <p
+            id="selected-file-name"
+            class="mt-2 text-sm text-gray-700 hidden"
+          >
+          </p>
+        </div>
+        <select
+          name="type"
+          class="select select-bordered flex-grow text-sm w-full"
+          required
+        >
+          <option class="text-gray-400" value="">
+            Prefere qual canal de resposta para seu orçamento?*
+          </option>
+          <option value="Elogio">E-mail</option>
+          <option value="Sugestão">WhatsApp</option>
+        </select>
+        <textarea
+          class="textarea textarea-bordered rounded-2xl min-h-80 w-full placeholder:text-accent"
+          name="notes"
+          placeholder="Observações*"
+          required
+        />
+        <button
+          class="btn btn-primary w-full"
+          type="submit"
+        >
+          <span class="[.htmx-request_&]:hidden inline">Enviar</span>
+          <span class="[.htmx-request_&]:inline-block hidden loading loading-spinner" />
+        </button>
+      </form>
+      <script
+        type="text/javascript"
+        defer
+        dangerouslySetInnerHTML={{
+          __html: useScript(onLoad),
+        }}
+      />
     </div>
   );
 }
@@ -206,14 +257,9 @@ export const action = async (
       //     },
       // }
     }
-    return {
-      toast: "success",
-      message: "Menssagem enviada com sucesso!",
-    };
+
+    return { status: "success" };
   } catch {
-    return {
-      toast: "error",
-      message: "Erro ao enviar a mensagem. Tente novamente mais tarde.",
-    };
+    return { status: "failed" };
   }
 };
