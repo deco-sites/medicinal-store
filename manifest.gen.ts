2f40029240db60cf4c45c882b358fbd7b6d1467e
wesley moraes <wesleymoraesserafim@gmail.com>
2025-09-11 11:17:21 -0300

    fix: adiciona BannerPdp

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -18,46 +18,47 @@ import * as $$$$$$1 from "./sections/Category/CategoryBanner.tsx";
 import * as $$$$$$2 from "./sections/Category/CategoryGrid.tsx";
 import * as $$$$$$3 from "./sections/Component.tsx";
 import * as $$$$$$4 from "./sections/Content/Alerts.tsx";
-import * as $$$$$$5 from "./sections/Content/Benefits.tsx";
-import * as $$$$$$6 from "./sections/Content/Brands.tsx";
-import * as $$$$$$7 from "./sections/Content/Cards.tsx";
-import * as $$$$$$8 from "./sections/Content/Citation.tsx";
-import * as $$$$$$9 from "./sections/Content/Faq.tsx";
-import * as $$$$$$10 from "./sections/Content/Hero.tsx";
-import * as $$$$$$11 from "./sections/Content/IconCards.tsx";
-import * as $$$$$$12 from "./sections/Content/Iframe.tsx";
-import * as $$$$$$13 from "./sections/Content/InfoCard.tsx";
-import * as $$$$$$14 from "./sections/Content/Intro.tsx";
-import * as $$$$$$15 from "./sections/Content/Logos.tsx";
-import * as $$$$$$16 from "./sections/Content/OnlyText.tsx";
-import * as $$$$$$17 from "./sections/Content/TextCard.tsx";
-import * as $$$$$$18 from "./sections/Content/TitleWithBackground.tsx";
-import * as $$$$$$19 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$20 from "./sections/Footer/Result.tsx";
-import * as $$$$$$21 from "./sections/Form/ContactUs.tsx";
-import * as $$$$$$22 from "./sections/Form/RDForm.tsx";
-import * as $$$$$$23 from "./sections/Form/SendYourRecipe.tsx";
-import * as $$$$$$24 from "./sections/Header/Header.tsx";
-import * as $$$$$$25 from "./sections/Images/Banner.tsx";
-import * as $$$$$$26 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$27 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$28 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$29 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$30 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$31 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$32 from "./sections/Product/Price.tsx";
-import * as $$$$$$33 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$34 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$35 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$36 from "./sections/Product/ProductShelfWithBanner.tsx";
-import * as $$$$$$37 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$38 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$39 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$40 from "./sections/Session.tsx";
-import * as $$$$$$41 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$42 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$44 from "./sections/temp.tsx";
-import * as $$$$$$43 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$5 from "./sections/Content/BannerPdp.tsx";
+import * as $$$$$$6 from "./sections/Content/Benefits.tsx";
+import * as $$$$$$7 from "./sections/Content/Brands.tsx";
+import * as $$$$$$8 from "./sections/Content/Cards.tsx";
+import * as $$$$$$9 from "./sections/Content/Citation.tsx";
+import * as $$$$$$10 from "./sections/Content/Faq.tsx";
+import * as $$$$$$11 from "./sections/Content/Hero.tsx";
+import * as $$$$$$12 from "./sections/Content/IconCards.tsx";
+import * as $$$$$$13 from "./sections/Content/Iframe.tsx";
+import * as $$$$$$14 from "./sections/Content/InfoCard.tsx";
+import * as $$$$$$15 from "./sections/Content/Intro.tsx";
+import * as $$$$$$16 from "./sections/Content/Logos.tsx";
+import * as $$$$$$17 from "./sections/Content/OnlyText.tsx";
+import * as $$$$$$18 from "./sections/Content/TextCard.tsx";
+import * as $$$$$$19 from "./sections/Content/TitleWithBackground.tsx";
+import * as $$$$$$20 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$21 from "./sections/Footer/Result.tsx";
+import * as $$$$$$22 from "./sections/Form/ContactUs.tsx";
+import * as $$$$$$23 from "./sections/Form/RDForm.tsx";
+import * as $$$$$$24 from "./sections/Form/SendYourRecipe.tsx";
+import * as $$$$$$25 from "./sections/Header/Header.tsx";
+import * as $$$$$$26 from "./sections/Images/Banner.tsx";
+import * as $$$$$$27 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$28 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$29 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$30 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$31 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$32 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$33 from "./sections/Product/Price.tsx";
+import * as $$$$$$34 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$35 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$36 from "./sections/Product/ProductShelfTabbed.tsx";
+import * as $$$$$$37 from "./sections/Product/ProductShelfWithBanner.tsx";
+import * as $$$$$$38 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$39 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$40 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$41 from "./sections/Session.tsx";
+import * as $$$$$$42 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$43 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$45 from "./sections/temp.tsx";
+import * as $$$$$$44 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -73,46 +74,47 @@ const manifest = {
     "site/sections/Category/CategoryGrid.tsx": $$$$$$2,
     "site/sections/Component.tsx": $$$$$$3,
     "site/sections/Content/Alerts.tsx": $$$$$$4,
-    "site/sections/Content/Benefits.tsx": $$$$$$5,
-    "site/sections/Content/Brands.tsx": $$$$$$6,
-    "site/sections/Content/Cards.tsx": $$$$$$7,
-    "site/sections/Content/Citation.tsx": $$$$$$8,
-    "site/sections/Content/Faq.tsx": $$$$$$9,
-    "site/sections/Content/Hero.tsx": $$$$$$10,
-    "site/sections/Content/IconCards.tsx": $$$$$$11,
-    "site/sections/Content/Iframe.tsx": $$$$$$12,
-    "site/sections/Content/InfoCard.tsx": $$$$$$13,
-    "site/sections/Content/Intro.tsx": $$$$$$14,
-    "site/sections/Content/Logos.tsx": $$$$$$15,
-    "site/sections/Content/OnlyText.tsx": $$$$$$16,
-    "site/sections/Content/TextCard.tsx": $$$$$$17,
-    "site/sections/Content/TitleWithBackground.tsx": $$$$$$18,
-    "site/sections/Footer/Footer.tsx": $$$$$$19,
-    "site/sections/Footer/Result.tsx": $$$$$$20,
-    "site/sections/Form/ContactUs.tsx": $$$$$$21,
-    "site/sections/Form/RDForm.tsx": $$$$$$22,
-    "site/sections/Form/SendYourRecipe.tsx": $$$$$$23,
-    "site/sections/Header/Header.tsx": $$$$$$24,
-    "site/sections/Images/Banner.tsx": $$$$$$25,
-    "site/sections/Images/Carousel.tsx": $$$$$$26,
-    "site/sections/Images/ImageGallery.tsx": $$$$$$27,
-    "site/sections/Images/ShoppableBanner.tsx": $$$$$$28,
-    "site/sections/Links/LinkTree.tsx": $$$$$$29,
-    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$30,
-    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$31,
-    "site/sections/Product/Price.tsx": $$$$$$32,
-    "site/sections/Product/ProductDetails.tsx": $$$$$$33,
-    "site/sections/Product/ProductShelf.tsx": $$$$$$34,
-    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$35,
-    "site/sections/Product/ProductShelfWithBanner.tsx": $$$$$$36,
-    "site/sections/Product/SearchResult.tsx": $$$$$$37,
-    "site/sections/Product/ShelfWithImage.tsx": $$$$$$38,
-    "site/sections/Product/Wishlist.tsx": $$$$$$39,
-    "site/sections/Session.tsx": $$$$$$40,
-    "site/sections/Social/InstagramPosts.tsx": $$$$$$41,
-    "site/sections/Social/WhatsApp.tsx": $$$$$$42,
-    "site/sections/temp.tsx": $$$$$$44,
-    "site/sections/Theme/Theme.tsx": $$$$$$43,
+    "site/sections/Content/BannerPdp.tsx": $$$$$$5,
+    "site/sections/Content/Benefits.tsx": $$$$$$6,
+    "site/sections/Content/Brands.tsx": $$$$$$7,
+    "site/sections/Content/Cards.tsx": $$$$$$8,
+    "site/sections/Content/Citation.tsx": $$$$$$9,
+    "site/sections/Content/Faq.tsx": $$$$$$10,
+    "site/sections/Content/Hero.tsx": $$$$$$11,
+    "site/sections/Content/IconCards.tsx": $$$$$$12,
+    "site/sections/Content/Iframe.tsx": $$$$$$13,
+    "site/sections/Content/InfoCard.tsx": $$$$$$14,
+    "site/sections/Content/Intro.tsx": $$$$$$15,
+    "site/sections/Content/Logos.tsx": $$$$$$16,
+    "site/sections/Content/OnlyText.tsx": $$$$$$17,
+    "site/sections/Content/TextCard.tsx": $$$$$$18,
+    "site/sections/Content/TitleWithBackground.tsx": $$$$$$19,
+    "site/sections/Footer/Footer.tsx": $$$$$$20,
+    "site/sections/Footer/Result.tsx": $$$$$$21,
+    "site/sections/Form/ContactUs.tsx": $$$$$$22,
+    "site/sections/Form/RDForm.tsx": $$$$$$23,
+    "site/sections/Form/SendYourRecipe.tsx": $$$$$$24,
+    "site/sections/Header/Header.tsx": $$$$$$25,
+    "site/sections/Images/Banner.tsx": $$$$$$26,
+    "site/sections/Images/Carousel.tsx": $$$$$$27,
+    "site/sections/Images/ImageGallery.tsx": $$$$$$28,
+    "site/sections/Images/ShoppableBanner.tsx": $$$$$$29,
+    "site/sections/Links/LinkTree.tsx": $$$$$$30,
+    "site/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$31,
+    "site/sections/Miscellaneous/CookieConsent.tsx": $$$$$$32,
+    "site/sections/Product/Price.tsx": $$$$$$33,
+    "site/sections/Product/ProductDetails.tsx": $$$$$$34,
+    "site/sections/Product/ProductShelf.tsx": $$$$$$35,
+    "site/sections/Product/ProductShelfTabbed.tsx": $$$$$$36,
+    "site/sections/Product/ProductShelfWithBanner.tsx": $$$$$$37,
+    "site/sections/Product/SearchResult.tsx": $$$$$$38,
+    "site/sections/Product/ShelfWithImage.tsx": $$$$$$39,
+    "site/sections/Product/Wishlist.tsx": $$$$$$40,
+    "site/sections/Session.tsx": $$$$$$41,
+    "site/sections/Social/InstagramPosts.tsx": $$$$$$42,
+    "site/sections/Social/WhatsApp.tsx": $$$$$$43,
+    "site/sections/temp.tsx": $$$$$$45,
+    "site/sections/Theme/Theme.tsx": $$$$$$44,
   },
   "actions": {
     "site/actions/minicart/submit.ts": $$$$$$$$$0,
---
+++ sections/Content/BannerPdp.tsx
@@ -0,0 +1,79 @@
+
+import type { ImageWidget } from "apps/admin/widgets.ts";
+interface BannerPdpProps {
+    /**
+* @title Imagem Mobile
+*/
+    mobile: BannerMobileProps;
+    /**
+ * @title Imagem Desktop
+ */
+    desktop: BannerDesktopProps;
+    /**
+* @title Link da imagem
+* @description URL para a qual o usuário será redirecionado ao clicar na imagem. 
+*/
+    link: string;
+}
+
+interface BannerDesktopProps {
+    /**
+ * @title Imagem Desktop
+ */
+    desktop: ImageWidget;
+    /**
+ * @title Texto Alternativo
+ */
+    alt: string;
+    /**
+ * @title Altura Máxima (px)
+ * @description Define a altura máxima da imagem em pixels. Se a imagem for maior que essa altura, ela será redimensionada proporcionalmente para caber dentro do limite definido. Se não for especificado, a imagem manterá sua altura original.
+ */
+    height?: number;
+}
+interface BannerMobileProps {
+    /**
+     * @title Imagem Mobile
+     * */
+    mobile: ImageWidget;
+    /**
+ * @title Texto Alternativo
+ */
+    alt: string;
+    /**
+     * @title Altura Máxima (px)
+     * @description Define a altura máxima da imagem em pixels. Se a imagem for maior que essa altura, ela será redimensionada proporcionalmente para caber dentro do limite definido. Se não for especificado, a imagem manterá sua altura original.
+     */
+    height?: number;
+}
+
+
+const BannerPdp = ({ mobile, desktop, link }: BannerPdpProps) => {
+    return (
+        <div class="flex justify-center my-4 container px-4 lg:px-0">
+            <a href={link}>
+                <picture>
+                    <source
+                        media="(max-width: 767px)"
+                        srcSet={mobile.mobile}
+                    />
+                    <source
+                        media="(min-width: 768px)"
+                        srcSet={desktop.desktop}
+                    />
+                    <img
+                        src={desktop.desktop}
+                        alt={desktop.alt || mobile.alt}
+                        style={{
+                            width: '100%',
+                            height: 'auto',
+                            maxHeight: `${desktop.height}px`
+                        }}
+                    />
+                </picture>
+            </a>
+        </div>
+    )
+}
+
+export default BannerPdp
